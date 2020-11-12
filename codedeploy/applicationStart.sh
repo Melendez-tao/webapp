3d2e1b9971cf480dd139e532c4af329a8b090e9a
Tao Wang <71620483+Melendez-tao@users.noreply.github.com>
2020-11-12 17:12:24 +0800

    A7 (#25)
    * Initial commit
    
    * delete old readme and gitignore
    
    * add unit test part
    
    * modify unittest
    
    * try fix unittest
    
    * fix unittest
    
    * fix unittest
    
    * fix request backend
    
    * Update test.js
    
    * Update test.js
    
    * fix the database mode
    
    * new assginment complete
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * try to buildand deploy
    
    * test in prod
    
    * test in prod
    
    * test in prod
    
    * test in prod
    
    * test in prod
    
    * test in prod
    
    * test in prod
    
    * test in prod
    
    * test in prod
    
    * test in prod
    
    * demo
    
    * test
    
    * fix
    
    * fix
    
    * fix

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-    -a fetch-config \
-    -m ec2 \
-    -c file:/opt/cloudwatch-config.json \
-    -s
+
 cd /var/webapp
 ls
 sudo npm install
 ls
- sudo node app.js > /dev/null 2> /dev/null < /dev/null &
+sudo node app.js > /dev/null 2> /dev/null < /dev/null &
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/var/webapp/cloudwatch-config.json \
+    -s
